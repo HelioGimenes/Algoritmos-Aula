826d0bfb6fcbc27800479ff5c292a90d1dc81777
Gimenes <helinhorad@gmail.com>
2024-09-12 20:36:17 -0300

    Delete Atividades directory

--- Atividades/num5a15.js
+++
@@ -1,7 +0,0 @@
-const prompt = require("prompt-sync")()
-
-let cont = 4
-for (i = 1; i <= 11; i++) {
-    cont++
-    console.log(`${cont}`)
-}
--- Atividades/tabuada.js
+++
@@ -1,6 +0,0 @@
-const prompt = require("prompt-sync")();
-
-const num = Number(prompt("Digite qual Tabuada: "));
-for (let i = 1; i <= 10; i++) {
-    console.log(`${num} x ${i} = ${num * i}`);
-}
