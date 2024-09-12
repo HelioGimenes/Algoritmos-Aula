b5e6dcd0be5943a514dce6dfb67860f7ceabb9b9
Gimenes <helinhorad@gmail.com>
2024-09-11 21:09:35 -0300

    Delete Atividades directory

--- Atividades/fatorial.js
+++
@@ -1,8 +0,0 @@
-const prompt = require("prompt-sync")();
-/*formula calculo fatorial 
-n! = n.(n-1.n-2)
-Ex n! de 5 = 5.4.3.2.1 = 120
-*/
-
-const num = Number(prompt("Digite um Número: "))
-
--- Atividades/numeros1a10.js
+++
@@ -1,7 +0,0 @@
-const prompt = require("prompt-sync")();
-
-let contador = 0
-for (let i = 1; i <= 10; i = i + 1) {
-    contador++
-    console.log(contador)
-}
