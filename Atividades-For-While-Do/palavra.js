5872bd96896ba8c730e5308b344e4b7707634f86
Gimenes <helinhorad@gmail.com>
2024-09-12 21:11:31 -0300

    Update palavra.js

--- Atividades-For-While-Do/palavra.js
+++ Atividades-For-While-Do/palavra.js
@@ -1,22 +1,22 @@
 const prompt = require("prompt-sync")()
 
-/* For */
+/* While */
 let i = 0
 while (i <= 10) {
     console.log("Programação")
     i++
 }
-console.log("For")
+console.log("While")
 
 
-/* While */
+/* For */
 /*
 let cont = 0
 for (i = 1; i <= 10; i++) {
     cont++
     console.log(`Programação.${cont}`)
     }
-    console.log ("While")
+    console.log ("For")
 
 
 /* Do...While */
