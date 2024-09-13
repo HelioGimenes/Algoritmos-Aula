cf63a6bb3a3bcecd61e2974a4718dd3319c93d35
Gimenes <helinhorad@gmail.com>
2024-09-12 22:22:12 -0300

    Update numero1a30.js

--- Atividades-For-While-Do/numero1a30.js
+++ Atividades-For-While-Do/numero1a30.js
@@ -4,8 +4,7 @@ let contador = 1
 while (contador <= 30){
     if (contador % 4 === 0) {
         contador ++;
-        continue;
-      }
+        }
       console.log(contador)
       contador++
     }
