82d8b055a65147df3cc8dc19d85896c3c6b326be
Gimenes <helinhorad@gmail.com>
2024-09-16 20:00:57 -0300

    Add files via upload

---
+++ Vetores/matriz3x3.js
@@ -0,0 +1,6 @@
+const prompt = require("prompt-sync")()
+
+let matriz = [
+    [1, 2, 3],[4, 5, 6],[7, 8, 9]];
+
+console.log("O número na segunda linha e terceira coluna é:", matriz[1][2]);
---
+++ Vetores/vetorInteiro.js
@@ -0,0 +1,9 @@
+let numeros = [10, 20, 30, 40, 50];
+let soma = 0;
+
+
+for (let i = 0; i < numeros.length; i++) {
+    soma += numeros[i];
+}
+
+console.log("A soma de todos os números é:", soma);
