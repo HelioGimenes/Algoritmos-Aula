ba0ecb126bb2a86644fa61111620631e64b86967
Gimenes <helinhorad@gmail.com>
2024-09-17 14:06:43 -0300

    Update vetorCores.js

--- Vetores/vetorCores.js
+++ Vetores/vetorCores.js
@@ -3,10 +3,12 @@ const prompt = require("prompt-sync")()
 const cor = prompt("Insira uma Cor: ")
 let cores = ["vermelho", "roxo", "preto"];
 
+let mostrar = (cores.indexOf(cor))
+
 if (!cores.includes(cor)) {
     console.log("Cor não encontrada.")
 
 }
 else {
-    console.log(`A cor escolhida foi:${cor}`)
+    console.log(`A cor escolhida foi:${cor} na Posição ${mostrar}`)
 }
