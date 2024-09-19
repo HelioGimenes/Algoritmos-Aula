8080dce2717f92d04af5fee9ac6b3853d8bd382f
Gimenes <helinhorad@gmail.com>
2024-09-19 14:25:18 -0300

    Update matrizSoma.js

--- Vetores/matrizSoma.js
+++ Vetores/matrizSoma.js
@@ -1,6 +1,12 @@
 const prompt = require("prompt-sync")()
 
 let numeros = [
-    [1, 2, 3],[4, 5, 6],[7, 8, 9]];
-let soma = numeros.map(numeros => numeros + numeros)
-console.log (soma)
+    [9, 8, 7], [6, 6, 6], [5, 4, 3]];
+let soma = 0
+
+for (let i = 0; i < numeros.length; i++) {
+    for (let j = 0; j < numeros.length; j++) {
+        soma += numeros[i][j];
+    }
+}
+console.log(soma)
