fb5ea88783e8a85d306233819f241070178481ea
Gimenes <helinhorad@gmail.com>
2024-09-21 11:30:38 -0300

    Update produtos.js

--- Vetores/produtos.js
+++ Vetores/produtos.js
@@ -6,6 +6,11 @@ let produtos = [
     { nome: "Carro", preco: 10000, categoria: "Veículo" },
     { nome: "Casa", preco: 15000, categoria: "Imóvel" },
     { nome: "Celular", preco: 5000, categoria: "Eletrônico" },
+    { nome: "Fones de Ouvido", preco: 200, categoria: "Eletrônico" },
+    { nome: "TV", preco: 1500, categoria: "Eletrônico" },
+    { nome: "Notebook", preco: 3000, categoria: "Eletrônico" },
+    { nome: "Camiseta", preco: 30, categoria: "Vestuário" },
+    { nome: "Calça", preco: 80, categoria: "Vestuário" }
 ];
 
 let produtosCaros = produtos.filter(produtos => produtos.preco > 100);
