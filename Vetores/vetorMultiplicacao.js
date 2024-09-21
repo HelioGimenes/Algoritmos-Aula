4beda0d8e2e8f766776fa5e27f2575aa360131b8
Gimenes <helinhorad@gmail.com>
2024-09-21 10:32:11 -0300

    Update vetorMultiplicacao.js

--- Vetores/vetorMultiplicacao.js
+++ Vetores/vetorMultiplicacao.js
@@ -1,8 +1,5 @@
 const prompt = require("prompt-sync")()
 
 let numeros = [10, 20, 30];
-
-numeros[0] = numeros[0] * 2;
-numeros[1] = numeros[1] * 2;
-numeros[2] = numeros[2] * 2;
-console.log(numeros)
+let mult = numeros.map(numero => numero * 2);
+console.log(mult)
